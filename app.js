ccf506a040431d8be73c08ecc98f4dfde3d7cd01
Florent VICENTE <vicentegroupe@gmail.com>
2020-03-22 16:14:53 +0100

    finishing touches with improving UX

--- app.js
+++ app.js
@@ -169,6 +169,12 @@ var UIController = (function () {
     return (type === "exp" ? "-" : "+") + " " + int + "." + dec
   }
 
+  var nodeListForEach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i)
+    }
+  }
+
   return {
     getInput: function () {
       return {
@@ -232,12 +238,6 @@ var UIController = (function () {
     displayPercentages: function (percentages) {
       var fields = document.querySelectorAll(DOMStrings.expensesPercLabel)
 
-      var nodeListForEach = function (list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i)
-        }
-      }
-
       nodeListForEach(fields, function (item, index) {
         if (percentages[index] > 0) {
           item.textContent = percentages[index] + "%"
@@ -256,6 +256,20 @@ var UIController = (function () {
 
       year = now.getFullYear()
       document.querySelector(DOMStrings.dateLabel).textContent = months[month] + " " + year
+    },
+
+    changeType: function () {
+      var fields = document.querySelectorAll(
+        DOMStrings.inputType + "," +
+        DOMStrings.inputDescription + "," +
+        DOMStrings.inputValue
+      )
+
+      nodeListForEach(fields, function (item) {
+        item.classList.toggle("red-focus")
+      })
+
+      document.querySelector(DOMStrings.inputBtn).classList.toggle("red")
     },
 
     getDOMStrings: function () {
@@ -281,6 +295,8 @@ var controller = (function (budgetCtrl, UICtrl) {
     })
 
     document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem)
+
+    document.querySelector(DOM.inputType).addEventListener("change", UICtrl.changeType)
   }
 
   var updateBudget = function () {
